00f13944ea8c35e35090b98b444d933126c7b5e7
connectwithakhil <akhilsharma90@gmail.com>
2020-04-23 02:56:56 +0530

    included request

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('../utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 app.set('view engine','hbs')
-app.set('views','/view')
+app.set('views','./view')
 const publicDir = path.join(__dirname,'../public')
 app.use(express.static(publicDir))
 
